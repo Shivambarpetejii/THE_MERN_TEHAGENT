70845898a68f0be357243b433f6790592e109120
Shivam Barpete <barpetejii@gmail.com>
2024-07-18 16:32:54 +0530

    this is mongo db conection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('connected the MongoDB!!');
+})
+.catch((err)=>{
+    console.log('ERROR!!');
+});
+//hPF71xV1dEMAMDcP
+//mongoose.connect("mongodb+srv://barpetejii:hPF71xV1dEMAMDcP@cluster14.4vcnmdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster14")
+
 
 const app = express();
 
 app.listen(3000,()=>{
-    console.log("server is runing on port number 3000!!");
+    console.log("server is runing on port number 3000!!!");
 })
